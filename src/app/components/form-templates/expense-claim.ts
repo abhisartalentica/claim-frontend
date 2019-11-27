3ab2e59a64b8dbc2b5226fa441fb27ecf3f9adb5
Abhisar Tripathi <abhisar.tripathi@talentica.com>
2019-11-27 11:41:15 +0530

    expance form first draft

--- src/app/components/form-templates/expense-claim.ts
+++ src/app/components/form-templates/expense-claim.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { EditableTableService } from "ng-editable-table/editable-table/editable-table.service";
 
 @Component({
-  selector: 'expence-claim',
-  templateUrl: './expence-claim.html',
-  styleUrls: ['../../app.component.css']
+  selector: "expence-claim",
+  templateUrl: "./expence-claim.html",
+  styleUrls: ["../../app.component.css"],
 })
 export class ExpenceClaimComponent implements OnInit {
+  tableHeaders = ["Header 1", "Header 2", "Header 3"];
+  tableRowsWithId = [[1, "Example", "Example", true]];
+  dataType = ["string", "string", "boolean"];
 
-  constructor() {console.log(10000) }
-
-  ngOnInit() {
+  constructor(private service: EditableTableService) {
+    // console.log(10000);
   }
 
+  ngOnInit() {
+    this.service.createTableWithIds(this.tableHeaders, this.tableRowsWithId, this.dataType);
+  }
 }
